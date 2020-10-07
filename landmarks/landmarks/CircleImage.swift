45a1618444d65164827fc32278d79e9269a4be31
luggy <luggyman123@gmail.com>
2020-10-06 20:59:36 -0700

    row and circle image

---
+++ landmarks/landmarks/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  landmarks
+//
+//  Created by Luqman Abdurrohman on 10/6/20.
+//  Copyright © 2020 Luqman Abdurrohman. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 10)
+        
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
---
+++ landmarks/landmarks/MapView.swift
@@ -0,0 +1,31 @@
+//
+//  MapView.swift
+//  landmarks
+//
+//  Created by Luqman Abdurrohman on 10/6/20.
+//  Copyright © 2020 Luqman Abdurrohman. All rights reserved.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: UIViewRepresentable {
+    func makeUIView(context: Contet) -> MKMapView{
+        MKMapView(frame: .zero)
+    }
+    
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+        let coordinate = CLLocationCoordinate2D(
+            latitude: 34.011286, longitude: -116.166868)
+        let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        uiView.setRegion(region, animated: true)
+    }
+    
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
