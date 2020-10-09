3686d0fe21334b108f175c65a4976ef48b6042b9
luggy <luggyman123@gmail.com>
2020-10-09 13:58:14 -0700

    stanforanford course

---
+++ CS193p_Memorize/CS193p_Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CS193p_Memorize
+//
+//  Created by Luqman Abdurrohman on 10/9/20.
+//  Copyright © 2020 Luqman Abdurrohman. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
--- landmarks/landmarks/LandmarkDetail.swift
+++ landmarks/landmarks/LandmarkDetail.swift
@@ -61,5 +61,6 @@ struct LandmarkDetail: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         LandmarkDetail(landmark: landmarkData[0])
+            .environmentObject(UserData())
     }
 }
